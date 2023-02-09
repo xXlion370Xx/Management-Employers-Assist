1d1e5a232aeb432d19629527d84098592d8a9ff3
Daniel Roa <lion35528@gmail.com>
2023-02-08 22:46:05 -0500

    Add fetch to backend to get response last date exist

--- model/managementAsist.js
+++ model/managementAsist.js
@@ -1,28 +1,21 @@
 const dateExist = (req, res) => {
     const userId = req.query.user;
     // const userId = 5;
-    let lastDataResult = ""
     req.getConnection((err, conn) => {
         if (err) {
-            console.log("Este es el error de con: " + err);
+            console.log("Error in get connection");
             console.log(err);
         }
 
         const sql = "SELECT * FROM asistencia WHERE id_usuario = ?";
         conn.query(sql, [userId], (err, data) => {
             if (err) {
-                console.log("Error en la consulta" + err);
+                console.log("Query error");
+                console.log(err);
             }
-
-            res.send('ok!');
-            console.log("Este es el ultimo registro: ");
-            console.log(data[data.length - 1]);
-            lastDataResult = data[data.length - 1];
+            res.send(data[data.length - 1]);
         });
-
     });
-
-    return lastDataResult;
 }
 
 module.exports = {
--- public/javascripts/home.js
+++ public/javascripts/home.js
@@ -1,11 +1,27 @@
-const dateExist = (url, idUser) => {
-    return result = fetch(url + "?user=" + idUser);
+async function dateExist(url, idUser) {
+    const response = await fetch(url + "?user=" + idUser);
+    return await response.json();
 }
 
 const url = "http://localhost:3000/users/userUpdate";
-const idUser = "3";
+const idUser = "28";
 
-dateExist(url, idUser);
+dateExist(url, idUser).then(data => {
+    console.log(data);
+    console.log(data.entrada);
+    console.log(data.salida);
+
+    if (data.entrada) {
+        console.log("Entrada exist");
+    }
+    if (data.salida) {
+        console.log("Salida exist");
+    }
+}).catch(err => {
+    console.log("Error in the query fetch");
+    console.log(err);
+});
+
 
 // TODO: Finish the validation of the last date input on db
 const buttonIn = document.getElementById('buttonIn');
