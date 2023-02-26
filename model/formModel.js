04c69b85313c14265ce4339e5f479a5a223e11a7
Daniel Roa <lion35528@gmail.com>
2023-02-26 13:12:35 -0500

    Add featuresend jwt to the front

--- model/formModel.js
+++ model/formModel.js
@@ -1,5 +1,6 @@
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcrypt');
+require('dotenv').config();
 
 const register = async (req, res) => {
     const { user, password } = req.body;
@@ -32,38 +33,66 @@ const register = async (req, res) => {
 
 }
 
-const login = (req, res) => {
-    const reqData = req.body;
+const login = async (req, res) => {
+    const { user, password } = req.body;
 
     req.getConnection((err, conn) => {
         if (err) {
-            console.log("No se pudo conectar a la base de datos debido a " + err);
+            console.log("Can´t connect to databsae due to: " + err);
             console.log(err);
         }
 
-        const sql = "SELECT * FROM users WHERE name = ? and password = ?";
-        conn.query(sql, [reqData.user, reqData.password], (err, data) => {
+        const sql = "SELECT * FROM users WHERE name = ?";
+        conn.query(sql, [user], (err, data) => {
             if (err) throw err;
 
             if (data.length === 0) {
-                res.render('login', { title: 'Iniciar Sesión', errorMessage: 'Algo salio mal' });
-                console.log("No se encontro registro con esa data: Obj-> " + data.length);
+                res.render('login', { title: 'Iniciar Sesión', errorMessage: 'Usuario no existe' });
+                console.log("No data with that info: " + data.length);
+
                 return;
             }
-            if (data[0].Usuario == reqData.user && data[0].Contraseña == reqData.password) {
-                res.render('home', { user: data[0].Usuario, rol: data[0].Tipo });
-            }
+
+            bcrypt.compare(password, data[0].password, (err, same) => {
+                if (err) {
+                    console.log("Something went wrong to compare the password with the hash due to: " + err);
+
+                    return;
+                }
+                if (!same) {
+                    res.render('login', { title: 'Inicio de Sesión', errorMessage: 'Contraseña incorrecta' });
+
+                    return;
+                }
+                generateToken(data[0], '1h').then(token => {
+                    console.log(token);
+
+                    res.cookie('token', token, {
+                        httpOnly: true,
+                        maxAge: 3600000 // 1 hour
+                    }).render('home', {
+                        user: data[0], user,
+                        rol: data[0].rol
+                    });
+
+                }).catch(err => {
+                    console.log("Can´t generate token due to: " + err);
+                })
+            });
         });
     });
 
 }
 
-const generateToken = (data) => {
+const generateToken = async (data, duration) => {
     const payload = {
         id: data.id,
-        user: data.user,
-        password: data.password
+        user: data.name,
+        password: data.password,
+        rol: data.rol
     }
+
+    return jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: duration });
 }
 module.exports = {
     register: register,
