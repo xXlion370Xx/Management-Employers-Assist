550607a65c0cda90dd4c1adb84a531365e45db34
xXlion370Xx <lion35528@gmail.com>
2023-07-24 11:28:35 -0500

    add configuration to connect db host planetscale

--- model/UTILS/dbConfig.js
+++ model/UTILS/dbConfig.js
@@ -3,9 +3,10 @@ const dbConfig = {
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT,
     database: process.env.DB_NAME,
-
+    ssl: {
+        rejectUnauthorized: true
+    }
 }
 
 module.exports = dbConfig;
