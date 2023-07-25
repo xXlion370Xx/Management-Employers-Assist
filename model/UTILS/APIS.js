c472bb4abadbd56dd50f01b2b2a1c02895430377
xXlion370Xx <lion35528@gmail.com>
2023-07-25 10:32:39 -0500

    fix function fecth

--- model/UTILS/APIS.js
+++ model/UTILS/APIS.js
@@ -1,4 +1,4 @@
-const jwt = require('jsonwebtoken');
+const fetch = require('node-fetch');
 
 const getCurrentLocalTime = async () => {
     try {
--- public/javascripts/featuresWorker.js
+++ public/javascripts/featuresWorker.js
@@ -3,7 +3,7 @@ async function getUserAssist(url) {
     return await response.json();
 }
 
-const url = "https://confeccioneslyz.onrender.com/users/asist";
+const url = "http://localhost:3000/users/asist";
 
 getUserAssist(url)
     .then(data => {
